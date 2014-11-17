5f5dbaafefac7775893d5d474f509d678e0d55e6
Boaz <boaz@n3rd.nl>
2014-11-17 20:25:45 +0100

    Refactorings

---
+++ Kudu.Web/Infrastructure/BasicAuthProxyCredentialProvider.cs
@@ -0,0 +1,56 @@
+﻿using Kudu.Client.Infrastructure;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Kudu.Web.Infrastructure
+{
+    public class BasicAuthProxyCredentialProvider : ICredentialProvider
+    {
+        private readonly ICredentials _credentials;
+
+        public BasicAuthProxyCredentialProvider(HttpContextBase context)
+        {
+            var networkCredentials = new NetworkCredential();
+
+            if (TryParseAuthHeader(context.Request.Headers, networkCredentials))
+            {
+                _credentials = networkCredentials;
+            }
+            else
+            {
+                _credentials = new NetworkCredential("admin", "kudu");
+            }
+        }
+
+        private static bool TryParseAuthHeader(NameValueCollection headers, NetworkCredential _credentials)
+        {
+            bool result = false;
+
+            string authHeader = headers["Authorization"];
+
+            if(!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Basic", StringComparison.OrdinalIgnoreCase))
+            {
+                string base64Credentials = authHeader.Substring(6);
+                string[] credentials = Encoding.ASCII.GetString(Convert.FromBase64String(base64Credentials)).Split(new char[] { ':' });
+                
+                if(credentials.Length == 2) 
+                {
+                    _credentials.UserName = credentials[0];
+                    _credentials.Password = credentials[1];
+
+                    result = true;
+                } 
+            }
+
+            return result;
+        }
+
+        public ICredentials GetCredentials()
+        {
+            return _credentials;
+        }
+    }
+}
